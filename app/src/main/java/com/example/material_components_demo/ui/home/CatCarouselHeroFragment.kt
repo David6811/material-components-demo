7ff2b12105eb3048d26183f5249dff5ae021094f
David <86584824+David6811@users.noreply.github.com>
2025-06-01 19:46:05 +1000

    Carousel (preparing) (#6)
    * add cat_carousel_hero_fragment.xml
    
    * preparing for carousel but waiting for update to Material3

---
+++ app/src/main/java/com/example/material_components_demo/ui/home/CatCarouselHeroFragment.kt
@@ -0,0 +1,18 @@
+package com.example.material_components_demo.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.material_components_demo.R
+
+class CatCarouselHeroFragment : Fragment(){
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.cat_carousel_hero_fragment, container, false)
+    }
+}
