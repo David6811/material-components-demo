ce7929d5b1f99d10abf9a5d956e410e8e4f90d34
David <86584824+David6811@users.noreply.github.com>
2025-06-01 16:43:32 +1000

    ! Cards (#5)
    * create cat_card_rich_media_demo_fragment.xml
    
    * Navigator to CatCardRichMediaDemoFragment when clicking cardsButton in HomeFragment

--- app/src/main/java/com/example/material_components_demo/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/material_components_demo/ui/home/HomeFragment.kt
@@ -1,10 +1,13 @@
 package com.example.material_components_demo.ui.home
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.material_components_demo.R
 import com.example.material_components_demo.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -25,6 +28,11 @@ class HomeFragment : Fragment() {
         actionBarManager = ActionBarManager(this)
         actionBarManager.setupActionBar(binding.textHome)
         bottomSheetManager.setupBottomSheet(inflater, container, binding.bottomsheetButton)
+
+        binding.cardsButton.setOnClickListener {
+            findNavController().navigate(R.id.catCardRichMediaDemoFragment)
+        }
+
         return root
     }
 
