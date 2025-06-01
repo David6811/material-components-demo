fd0106c7b68cb35fad76d8903d0778358c20aebb
weixu <xuwei19850423@gmail.com>
2025-06-01 14:34:56 +1000

    extract ActionBarManager

--- app/src/main/java/com/example/material_components_demo/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/material_components_demo/ui/home/HomeFragment.kt
@@ -4,21 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.view.ActionMode
 import androidx.fragment.app.Fragment
-import com.example.material_components_demo.R
 import com.example.material_components_demo.databinding.FragmentHomeBinding
-import android.view.Menu
-import android.view.MenuItem
 
 class HomeFragment : Fragment() {
+
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
-    private var actionMode: ActionMode? = null
     private lateinit var bottomSheetManager: BottomSheetManager
-
+    private lateinit var actionBarManager: ActionBarManager
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,58 +21,17 @@ class HomeFragment : Fragment() {
     ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
-        toggleActionBar()
         bottomSheetManager = BottomSheetManager(this)
+        actionBarManager = ActionBarManager(this)
+        actionBarManager.setupActionBar(binding.textHome)
         bottomSheetManager.setupBottomSheet(inflater, container, binding.bottomsheetButton)
-
         return root
-    }
-
-    private fun toggleActionBar() {
-        val toggleActionModeButton: Button = binding.textHome
-
-        //        val homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
-        //        homeViewModel.text.observe(viewLifecycleOwner) {
-        //            toggleActionModeButton.text = it
-        //        }
-
-        toggleActionModeButton.setOnClickListener {
-            if (actionMode == null) {
-                actionMode =
-                    (activity as AppCompatActivity).startSupportActionMode(actionModeCallback)
-                actionMode?.title = "1 selected"
-            }
-        }
-    }
-
-    private val actionModeCallback = object : ActionMode.Callback {
-        override fun onCreateActionMode(mode: ActionMode?, menu: Menu?): Boolean {
-            mode?.menuInflater?.inflate(R.menu.contextual_action_bar, menu)
-            return true
-        }
-
-        override fun onPrepareActionMode(mode: ActionMode?, menu: Menu?): Boolean {
-            return false
-        }
-
-        override fun onActionItemClicked(mode: ActionMode?, item: MenuItem?): Boolean {
-            return when (item?.itemId) {
-                R.id.delete, R.id.more -> {
-                    mode?.finish()
-                    true
-                }
-                else -> false
-            }
-        }
-
-        override fun onDestroyActionMode(mode: ActionMode?) {
-            actionMode = null
-        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         bottomSheetManager.cleanup()
+        actionBarManager.cleanup()
         _binding = null
     }
 }
