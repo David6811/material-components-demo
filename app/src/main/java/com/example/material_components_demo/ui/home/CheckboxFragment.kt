33d9760fb1e1c37a6e1b62c0bd5d61fdc652b17c
weixu <xuwei19850423@gmail.com>
2025-06-02 09:26:24 +1000

    add CheckboxFragment

---
+++ app/src/main/java/com/example/material_components_demo/ui/home/CheckboxFragment.kt
@@ -0,0 +1,19 @@
+package com.example.material_components_demo.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.material_components_demo.R
+
+class CheckboxFragment : Fragment(){
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.cat_checkbox, container, false)
+    }
+
+}
