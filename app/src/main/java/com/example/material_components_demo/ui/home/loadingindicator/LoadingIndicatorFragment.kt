6354a0cd420bef47142276f9145e75568afde7aa
David <86584824+David6811@users.noreply.github.com>
2025-06-03 10:33:49 +1000

    Loadingindicator(todo) (#17)
    * add cat_loading_indicator_fragment.xml
    
    * CircularProgressIndicator

---
+++ app/src/main/java/com/example/material_components_demo/ui/home/loadingindicator/LoadingIndicatorFragment.kt
@@ -0,0 +1,19 @@
+package com.example.material_components_demo.ui.home.loadingindicator
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.material_components_demo.R
+
+class LoadingIndicatorFragment : Fragment(){
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.cat_loading_indicator_fragment, container, false)
+    }
+
+}
